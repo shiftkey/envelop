240da92c1552b18224dce5fac88aeaa4a98eb355
Brendan Forster <me@brendanforster.com>
2012-06-12 15:42:04 +0930

    implementing script to build packages using psake, installing the dev certification (requires local admin) and running tests from the created appx

---
+++ build/Install-Certificate.ps1
@@ -0,0 +1,11 @@
+﻿function Get-ScriptDirectory
+{
+    $Invocation = (Get-Variable MyInvocation -Scope 1).Value
+    Split-Path $Invocation.MyCommand.Path
+}
+
+$ScriptDir = (Get-ScriptDirectory)
+$BaseDir = Join-Path $ScriptDir ".."
+$pfx = "$BaseDir\Cocoon.Tests\Cocoon.Tests_TemporaryKey.pfx"
+$pfx
+Certutil -importpfx "$pfx"
--- build/Package.ps1
+++ build/Package.ps1
@@ -1,11 +1,113 @@
-param([Parameter(Mandatory=$true)][string]$Version)
+properties {
+    $ScriptDir = Resolve-Path "."
+    $BaseDir = Join-Path $ScriptDir ".."
+    $SolutionFile = "$BaseDir\Cocoon.sln"
+    $OutputDir = "$BaseDir\Deploy\Package\"
+    $ArtifactsDir = "$BaseDir\artifacts\"
+    $NuGetPackDir = Join-Path "$OutputDir" "Pack\"
+    $BaseVersion = "1.0.1"
+    $Version = "$BaseVersion-rc" 
+    $PatchNumber = (git rev-list --all | wc -l).trim() # TODO: better implementation
+    $AssemblyVersion = "$BaseVersion.$PatchNumber"
+}
 
-New-Item ..\artifacts -type directory -Force
+function Log-Message ($message) {
+    write-host $message -foregroundcolor "green"
+}
 
-..\.nuget\NuGet.exe pack ..\Cocoon\Cocoon.csproj -Prop Configuration=Release -Output ..\artifacts -Version $Version
+function Set-Version {
+    param([Parameter(Mandatory=$true)][string]$assembly_info,[Parameter(Mandatory=$true)][string]$Version)
 
-..\.nuget\NuGet.exe pack ..\Cocoon.Data\Cocoon.Data.csproj -Prop Configuration=Release -Output ..\artifacts -Version $Version
+    $infile=get-content $assembly_info
+    $regex = New-Object System.Text.RegularExpressions.Regex "\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b"
+    $replace = $regex.Replace($infile,$version)
+    set-content -Value $replace $assembly_info
+}
 
-..\.nuget\NuGet.exe pack ..\Cocoon.MEF\Cocoon.MEF.csproj -Prop Configuration=Release -Output ..\artifacts -Version $Version
+function Create-IfNotFound {
+    param([Parameter(Mandatory=$true)][string]$Directory)
 
-..\.nuget\NuGet.exe pack ..\Cocoon.Autofac\Cocoon.Autofac.csproj -Prop Configuration=Release -Output ..\artifacts -Version $Version
+    if ((Test-Path $Directory -PathType Container) -ne $true) {
+        mkdir $Directory | out-null 
+    }
+}
+
+function Clear-Recursively {
+    param([Parameter(Mandatory=$true)][string]$Directory)
+
+    if (Test-Path $Directory -PathType Container) {
+        ri $Directory -Recurse | out-null 
+    }
+}
+
+function Create-Package {
+    param([Parameter(Mandatory=$true)][string]$PackageName,[Parameter(Mandatory=$true)][string]$TempDir)
+
+    Create-IfNotFound $TempDir
+    Create-IfNotFound $ArtifactsDir
+    Create-IfNotFound "$TempDir\lib\winrt45\"
+
+    $PackageNameFile = "$PackageName.nuspec"
+    $nuspecFile = "$BaseDir\build\$PackageName\$PackageName.nuspec"
+
+    cp $nuspecFile $TempDir
+    cp "$OutputDir\$PackageName\$PackageName.dll" "$TempDir\lib\winrt45\" # add initial assembly
+    rm "$OutputDir\$PackageName\Cocoon.*.dll" # remove assemblies available from other Cocoon packages
+    cp "$OutputDir\$PackageName\*.dll" "$TempDir\lib\winrt45\" # include other binary dependencies
+
+    if (Test-Path "$PackageName\tools\") { # import any PS scripts for this package
+        cp "$PackageName\tools\*" "$TempDir\tools\"
+    }
+   
+    Log-Message "Modifying placeholder in nuspec file"
+    $Spec = [xml](get-content "$TempDir\$PackageNameFile")
+    $Spec.package.metadata.version = ([string]$Spec.package.metadata.version).Replace("{version}",$Version)
+    foreach($dependency in $Spec.package.metadata.dependencies.dependency) { 
+        $dependency.version = ([string]$dependency.version).Replace("{version}",$Version)
+    } 
+
+    $Spec.Save("$TempDir\$PackageNameFile")
+    exec { ..\.nuget\NuGet.exe pack "$TempDir\$PackageNameFile" -Output $ArtifactsDir }
+    Clear-Recursively $TempDir
+}
+
+task default -depends test
+
+task clean {
+    Log-Message "Clearing tracked and untracked files"
+    git checkout -- $BaseDir
+    git clean -df $BaseDir
+    Clear-Recursively $OutputDir
+    Clear-Recursively $ArtifactsDir
+}
+
+task version -depends clean {
+    Log-Message "Updating version to $version"
+    foreach ($assemblyinfo in $(get-childitem -Path $BaseDir -Include "AssemblyInfo.cs" -recurse)) {
+        Set-Version -assembly_info "$assemblyinfo" -Version $AssemblyVersion
+    }
+}   
+
+task compile -depends clean,version  {
+    msbuild $SolutionFile "/p:OutDir=$OutputDir" "/p:Configuration=Release"  "/verbosity:quiet"
+}
+
+task test -depends compile {
+    $path = 'C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe'
+    $appx = "$OutputDir\Cocoon.Tests\AppPackages\Cocoon.Tests_1.0.0.0_AnyCPU_Test\Cocoon.Tests_1.0.0.0_AnyCPU.appx"
+    if (Test-Path $path) {
+        start-process $path -ArgumentList "$appx `/InIsolation" -NoNewWindow -Wait
+    }
+}
+
+task package -depends compile,test {
+    Create-Package -PackageName "Cocoon" -TempDir $NuGetPackDir
+    Create-Package -PackageName "Cocoon.Data" -TempDir $NuGetPackDir
+    Create-Package -PackageName "Cocoon.MEF" -TempDir $NuGetPackDir
+    Create-Package -PackageName "Cocoon.Autofac" -TempDir $NuGetPackDir
+}
+
+task release -depends package {
+    Log-Message "Clearing tracked files"
+    git checkout -- $BaseDir
+}
